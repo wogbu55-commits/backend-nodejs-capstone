9cf27f80a517c9288a2196f9504a4f9f3e0e48d1
Domaintheman <wogbu55@gmail.com>
2026-08-23 23:38:10 +0100

    Refactor loadData function in import-mongo
    Refactor loadData function for improved structure and error handling.

--- secondChance-backend/util/import-mongo/index.js
+++ secondChance-backend/util/import-mongo/index.js
@@ -12,35 +12,33 @@ const collectionName = 'secondChanceItems'
 const data = JSON.parse(fs.readFileSync(filename, 'utf8')).docs
 
 // connect to database and insert data into the collection
-async function loadData() {
-    const client = new MongoClient(url)
+async function loadData () {
+  const client = new MongoClient(url)
+  try {
+    // Connect to the MongoDB client
+    await client.connect()
+    console.log('Connected successfully to server')
+    // database will be created if it does not exist
+    const db = client.db(dbName)
 
-    try {
-        // Connect to the MongoDB client
-        await client.connect()
-        console.log('Connected successfully to server')
+    // collection will be created if it does not exist
+    const collection = db.collection(collectionName)
+    let cursor = await collection.find({})
+    let documents = await cursor.toArray()
 
-        // database will be created if it does not exist
-        const db = client.db(dbName)
-
-        // collection will be created if it does not exist
-        const collection = db.collection(collectionName)
-        let cursor = await collection.find({})
-        let documents = await cursor.toArray()
-
-        if(documents.length == 0) {
-            // Insert data into the collection
-            const insertResult = await collection.insertMany(data)
-            console.log('Inserted documents:', insertResult.insertedCount)
-        } else {
-            console.log('Items already exists in DB')
-        }
-    } catch (err) {
-        console.error(err)
-    } finally {
-        // Close the connection
-        await client.close()
+    if(documents.length == 0) {
+      // Insert data into the collection
+      const insertResult = await collection.insertMany(data)
+      console.log('Inserted documents:', insertResult.insertedCount)
+    } else {
+      console.log('Items already exists in DB')
     }
+  } catch (err) {
+    console.error(err)
+  } finally {
+    // Close the connection
+    await client.close()
+  }
 }
 
 loadData()
