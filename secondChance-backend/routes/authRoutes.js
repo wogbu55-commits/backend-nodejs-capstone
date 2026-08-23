bf1e021bbd496904f5bb58edb33b7738ed98e743
wogbu55-commits <wogbu55@gmail.com>
2026-08-23 16:40:24 +0100

    login users

--- secondChance-backend/routes/authRoutes.js
+++ secondChance-backend/routes/authRoutes.js
@@ -54,4 +54,43 @@ router.post('/register', async (req, res) => {
     }
 });
 
+router.post('/login', async (req, res) => {
+    try {
+        // Task 1: Connect to `secondChance` in MongoDB through `connectToDatabase` in `db.js`.
+        const db = await connectToDatabase();
+        // Task 2: Access MongoDB `users` collection
+        const collection =db.collection("users");
+        // Task 3: Check for user credentials in database
+        const theUser = await collection.findOne({ email: req.body.email });
+
+        if (!theUser) {
+            logger.error(`User with email ${req.body.email} not found`);
+            return res.status(400).json({error: 'Invalid credentials'});
+        }
+        // Task 4: Check if the password matches the encrypted password and send appropriate message on mismatch
+        if (theUser) {
+            let result = await bcrypt.compare(req.body.password, theUser.password);
+            if (!result) {
+                logger.error(`Invalid password for user with email ${req.body.email}`);
+                return res.status(400).json({error: 'Invalid credentials'});
+            }
+        }
+        // Task 5: Fetch user details from a database
+        const userName = theUser.firstName;
+        const userEmail = theUser.email;
+        // Task 6: Create JWT authentication if passwords match with user._id as payload
+        const payload = {
+            user: {
+                id: theUser._id.toString(),
+            },
+        };
+        const authtoken = jwt.sign(payload, jwtSecret);
+        res.json({authtoken, userName, userEmail });
+        // Task 7: Send appropriate message if the user is not found
+    } catch (e) {
+         return res.status(500).send('Internal server error');
+
+    }
+});
+
 module.exports = router;
