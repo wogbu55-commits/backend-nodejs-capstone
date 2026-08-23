30e8e5773c325dc9882559f9bbf011b6b461b557
Domaintheman <wogbu55@gmail.com>
2026-08-23 23:24:41 +0100

    Fix regex options in search query filter

--- secondChance-backend/routes/searchRoutes.js
+++ secondChance-backend/routes/searchRoutes.js
@@ -15,7 +15,7 @@ router.get('/', async (req, res, next) => {
 
     // Add the name filter to the query if the name parameter is not empty
     if (req.query.name && req.query.name.trim() !== '') {
-      query.name = { $regex: req.query.name, $options: "i" } // Using regex for partial match, case-insensitive
+      query.name = { $regex: req.query.name, $options: 'i' } // Using regex for partial match, case-insensitive
     }
 
     // Task 3: Add other filters to the query
