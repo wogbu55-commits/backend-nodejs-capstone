096b868719960628481f802bed551b3e99f5fe0b
Domaintheman <wogbu55@gmail.com>
2026-08-24 00:09:54 +0100

    Update index.js

--- sentiment/index.js
+++ sentiment/index.js
@@ -45,7 +45,7 @@ app.post('/sentiment', async (req, res) => {
     logger.info(`Sentiment analysis result: ${analysisResult}`)
 
     // Task 6: send a status code of 200 with both sentiment score and the sentiment txt in the format { sentimentScore: analysisResult, sentiment: sentiment }
-    res.status(200).json({ sentimentScore: 'analysisResult, sentiment' })
+    res.status(200).json({ sentimentScore: 'analysisResult', sentiment: 'sentiment' })
   } catch (e) {
     logger.error(`Error performing sentiment analysis: ${e}`)
     // Task 7: if there is an error, return a HTTP code of 500 and the json {'message': 'Error performing sentiment analysis'}
