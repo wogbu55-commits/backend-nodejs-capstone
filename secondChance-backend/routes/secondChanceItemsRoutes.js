9f5129707b961fe326179fbc3e57a812bc90530a
Domaintheman <wogbu55@gmail.com>
2026-08-23 23:07:39 +0100

    Update secondChanceItemsRoutes.js

--- secondChance-backend/routes/secondChanceItemsRoutes.js
+++ secondChance-backend/routes/secondChanceItemsRoutes.js
@@ -83,7 +83,7 @@ router.put('/:id', async (req, res, next) => {
     secondChanceItem.age_days = req.body.age_days
     secondChanceItem.description = req.body.description
     secondChanceItem.age_years = Number((secondChanceItem.age_days / 365).toFixed(1))
-    secondChanceItem.updateAt = new Date()const updatepreloveItem = await collection.findOneAndUpdate(
+    secondChanceItem.updateAt = new Date()  const updatepreloveItem = await collection.findOneAndUpdate(
       { id: req.params.id },
       { $set: secondChanceItem },
       { returnDocument: 'after' }
