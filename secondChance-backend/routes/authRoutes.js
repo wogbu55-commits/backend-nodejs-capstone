d531bf6e6f23da124336bec9104927d29b145473
wogbu55-commits <wogbu55@gmail.com>
2026-08-23 15:59:09 +0100

    auth users

---
+++ secondChance-backend/routes/authRoutes.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const router = express.Router();
+const connectToDatabase = require('../models/db');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const logger = require('../logger');
+
+const jwtSecret = process.env.JWT_SECRET;
+
+router.post('/register', async (req, res) => {
+    try {
+        // Task 1: Connect to `secondChance` in MongoDB through `connectToDatabase` in `db.js`.
+        const db = await connectToDatabase();
+        // Task 2: Access MongoDB `users` collection
+        const collection = db.collection("users");
+        // Task 3: Check if user credentials already exists in the database and throw an error if they do
+        const existingemail = await collection.findOne({ email: req.body.email });
+
+        if (existingemail) {
+            logger.error(`User with email ${req.body.email} already exists`);
+            return res.status(400).json({error: 'User with this email already exits'});
+        }
+        // Task 4: Create a hash to encrypt the password so that it is not readable in the database
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(req.body.password, salt);
+        // Task 5: Insert the user into the database
+        const newUser = await collection.insertOne({
+            email: req.body.email,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            password: hash,
+            createdAt: new Date(),
+        });
+        // Task 6: Create JWT authentication if passwords match with user._id as payload
+        const payload = {
+            user: {
+                id: newUser.insertedId,
+            },
+        };
+
+        const authToken = jwt.sign(payload, jwtSecret);
+        // Task 7: Log the successful registration using the logger
+        logger.info('User registered successfully');
+        // Task 8: Return the user email and the token as a JSON
+        res.json({ authToken, email: req.body.email });
+    } catch (e) {
+         logger.error(e);
+         return res.status(500).send('Internal server error');
+    }
+});
+
+module.exports = router;
