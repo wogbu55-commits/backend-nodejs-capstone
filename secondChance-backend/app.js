11738efe82bdd3c4e8059ae2152ae81dccf92866
Domaintheman <wogbu55@gmail.com>
2026-08-23 21:47:23 +0100

    Refactor app.js for consistency and style

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -1,60 +1,58 @@
 /*jshint esversion: 8 */
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
-const pinoLogger = require('./logger');
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const path = require('path')
+const pinoLogger = require('./logger')
 
-const connectToDatabase = require('./models/db');
-const {loadData} = require("./util/import-mongo/index");
+const connectToDatabase = require('./models/db')
 
-
-const app = express();
-app.use("*",cors());
-const port = 3060;
+const app = express()
+app.use("*",cors())
+const port = 3060
 
 // Connect to MongoDB; we just do this one time
 connectToDatabase().then(() => {
-    pinoLogger.info('Connected to DB');
+    pinoLogger.info('Connected to DB')
 })
-    .catch((e) => console.error('Failed to connect to DB', e));
+    .catch((e) => console.error('Failed to connect to DB', e))
 
 
-app.use(express.json());
-app.use('/images', express.static(path.join(__dirname, 'public/images')));
+app.use(express.json())
+app.use('/images', express.static(path.join(__dirname, 'public/images')))
 
 // Route files
 
 // authRoutes Step 2: import the authRoutes and store in a constant called authRoutes
-const authRoutes = require('./routes/authRoutes');
+const authRoutes = require('./routes/authRoutes')
 
 // Items API Task 1: import the secondChanceItemsRoutes and store in a constant called secondChanceItemsRoutes
-const secondChanceItemsRoutes = require('./routes/secondChanceItemsRoutes');
+const secondChanceItemsRoutes = require('./routes/secondChanceItemsRoutes')
 
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
-const searchRoutes = require('./routes/searchRoutes');
+const searchRoutes = require('./routes/searchRoutes')
 
 
-const pinoHttp = require('pino-http');
-const logger = require('./logger');
+const pinoHttp = require('pino-http')
+const logger = require('./logger')
 
-app.use(pinoHttp({ logger }));
+app.use(pinoHttp({ logger }))
 
 // Use Routes
 // authRoutes Step 2: add the authRoutes and to the server by using the app.use() method.
-app.use('/api/auth', authRoutes);
+app.use('/api/auth', authRoutes)
 
 // Items API Task 2: add the secondChanceItemsRoutes to the server by using the app.use() method.
-app.use('/api/secondchance/items', secondChanceItemsRoutes);
+app.use('/api/secondchance/items', secondChanceItemsRoutes)
 
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
-app.use('/api/secondchance/search', searchRoutes);
+app.use('/api/secondchance/search', searchRoutes)
 
 
 // Global Error Handler
 app.use((err, req, res, next) => {
     console.error(err);
-    res.status(500).send('Internal Server Error');
+    res.status(500).send('Internal Server Error')
 });
 
 app.get("/",(req,res)=>{
@@ -62,5 +60,5 @@ app.get("/",(req,res)=>{
 })
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+    console.log(`Server running on port ${port}`)
 });
